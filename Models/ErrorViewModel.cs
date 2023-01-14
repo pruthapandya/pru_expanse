603e94139483d46560fb4efd5f0f5a426acfefd9
HP <HP@LAPTOP-52QE9I9H>
2023-01-14 22:18:03 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pru_expanse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
